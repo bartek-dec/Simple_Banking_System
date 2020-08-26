40e3b02bd4cac13fe5b06c8a3a883d994aa01b71
Bartek Dec <decbartek@gmail.com>
2020-08-26 20:48:00 +0200

    updated main() method

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -1,13 +1,18 @@
 package com.example;
 
 import com.example.service.Bank;
+import com.example.util.Generator;
+import com.example.util.NumberGenerator;
 
+import java.util.Random;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        Bank bank = new Bank(scanner);
+        Random random = new Random();
+        Generator generator = new NumberGenerator(random);
+        Bank bank = new Bank(scanner,generator);
 
         bank.runSystem();
     }
