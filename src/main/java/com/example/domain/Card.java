8b52a65f333df364bd77dac940eb4370abebd907
Bartek Dec <decbartek@gmail.com>
2020-08-26 13:59:25 +0200

    added Card interface

---
+++ src/main/java/com/example/domain/Card.java
@@ -0,0 +1,12 @@
+package com.example.domain;
+
+public interface Card {
+
+    void setCardNumber(String number);
+
+    String getCardNumber();
+
+    void setPIN(String pin);
+
+    String getPIN();
+}
