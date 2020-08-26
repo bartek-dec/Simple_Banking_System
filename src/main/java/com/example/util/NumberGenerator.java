829ba97aee9aed74c5635e4594aa45ad770b9b25
Bartek Dec <decbartek@gmail.com>
2020-08-26 18:32:54 +0200

    modified getCardNumber() method so that it returns only unique numbers

--- src/main/java/com/example/util/NumberGenerator.java
+++ src/main/java/com/example/util/NumberGenerator.java
@@ -1,24 +1,33 @@
 package com.example.util;
 
+import java.util.HashSet;
 import java.util.Random;
+import java.util.Set;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 public class NumberGenerator implements Generator {
 
+    private final Set<String> cardNumbers;
     private final String IIN = "400000";
 
     private Random random;
 
     public NumberGenerator(Random random) {
         this.random = random;
+        cardNumbers = new HashSet<>();
     }
 
     @Override
     public String getCardNumber() {
-        return IIN + IntStream.range(0, 10)
-                .mapToObj(e -> String.valueOf(random.nextInt(10)))
-                .collect(Collectors.joining(""));
+        String cardNumber;
+        do {
+            cardNumber = IIN + IntStream.range(0, 10)
+                    .mapToObj(e -> String.valueOf(random.nextInt(10)))
+                    .collect(Collectors.joining(""));
+        } while (!cardNumbers.add(cardNumber));
+
+        return cardNumber;
     }
 
     @Override
