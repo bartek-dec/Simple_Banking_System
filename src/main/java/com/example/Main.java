9253f4c53c6161b979c03ea774a652184debe465
Bartek Dec <decbartek@gmail.com>
2020-08-26 14:09:35 +0200

    added scanner and bank objects in the main() method

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -1,7 +1,14 @@
 package com.example;
 
+import com.example.service.Bank;
+
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Bank bank = new Bank(scanner);
 
+        bank.runSystem();
     }
 }
