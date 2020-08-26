b77313d6a7b68d3019b8b1bce03c5115de90558d
Bartek Dec <decbartek@gmail.com>
2020-08-26 20:47:17 +0200

    implemented logic in the Bank class

--- src/main/java/com/example/service/Bank.java
+++ src/main/java/com/example/service/Bank.java
@@ -1,29 +1,62 @@
 package com.example.service;
 
+import com.example.domain.Account;
+import com.example.domain.BankAccount;
+import com.example.domain.Card;
+import com.example.domain.CreditCard;
+import com.example.util.Generator;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
 import java.util.Scanner;
 
 public class Bank {
 
+    private List<Account> accounts;
     private Scanner scanner;
+    private Generator generator;
 
-    public Bank(Scanner scanner) {
+    public Bank(Scanner scanner, Generator generator) {
         this.scanner = scanner;
+        this.generator = generator;
+        this.accounts = new ArrayList<>();
     }
 
     public void runSystem() {
         int option;
-
+        Account account;
         do {
             mainMenu();
             option = getUserInput();
             switch (option) {
                 case 1:
-                    System.out.println("option 1");
-                    //createAccount();
+                    account = createAccount();
+                    accounts.add(account);
+                    System.out.println(displayAccount(account));
                     break;
                 case 2:
-                    System.out.println("option 2");
-                    //logIntoAccount();
+                    account = logIntoAccount();
+
+                    if (account != null) {
+                        System.out.println("\nYou have successfully logged in!\n");
+
+                        do {
+                            displayLogInSubmenu();
+                            option = getUserInput();
+
+                            switch (option) {
+                                case 1:
+                                    showBalance(account);
+                                    break;
+                                case 2:
+                                    System.out.println("\nYou have successfully logged out!\n");
+                                    break;
+                                case 0:
+                                    break;
+                            }
+                        } while (option == 1);
+                    }
                     break;
                 case 0:
                     break;
@@ -32,12 +65,18 @@ public class Bank {
             }
         } while (option != 0);
 
-        System.out.println("Bye!");
+        System.out.println("\nBye!");
     }
 
-    private static void mainMenu() {
-        System.out.println("\n1. Create an account");
+    private void mainMenu() {
+        System.out.println("1. Create an account");
         System.out.println("2. Log into account");
+        System.out.println("0. Exit");
+    }
+
+    private void displayLogInSubmenu() {
+        System.out.println("1. Balance");
+        System.out.println("2. Log out");
         System.out.println("0. Exit");
     }
 
@@ -55,5 +94,62 @@ public class Bank {
         }
 
         return input;
+    }
+
+    private Account createAccount() {
+        Card card = createCard();
+        return new BankAccount(card);
+    }
+
+    private Card createCard() {
+        String cardNumber = generator.getCardNumber();
+        String pin = generator.getPIN();
+
+        return new CreditCard(cardNumber, pin);
+    }
+
+    private String displayAccount(Account account) {
+        StringBuilder builder = new StringBuilder();
+
+        builder.append("\nYour card has been created\n");
+        builder.append("Your card number:\n");
+        builder.append(account.getCard().getCardNumber() + "\n");
+        builder.append("Your card PIN:\n");
+        builder.append(account.getCard().getPIN() + "\n");
+
+        return builder.toString();
+    }
+
+    private Account logIntoAccount() {
+        System.out.println("\nEnter your card number:");
+        String cardNumber = scanner.nextLine().trim();
+
+        System.out.println("Enter your PIN:");
+        String pin = scanner.nextLine().trim();
+
+        Account account = findAccount(cardNumber);
+
+        if (account == null) {
+            System.out.println("\nWrong card number or PIN!\n");
+            return null;
+        } else if (Objects.equals(account.getCard().getPIN(), pin)) {
+            return account;
+        }
+
+        return null;
+    }
+
+    private Account findAccount(String cardNumber) {
+        if (cardNumber == null) {
+            return null;
+        }
+
+        return accounts.stream()
+                .filter(e -> Objects.equals(e.getCard().getCardNumber(), cardNumber))
+                .findFirst().orElse(null);
+    }
+
+    private void showBalance(Account account) {
+        System.out.println("\nBalance: " + account.getBalance() + "\n");
     }
 }
