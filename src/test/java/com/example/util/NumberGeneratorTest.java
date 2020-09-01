28c92ae2ae65591908a87600c1cb3553537dcb6f
Bartek Dec <decbartek@gmail.com>
2020-09-01 18:20:00 +0200

    updated test class by adding CardService

--- src/test/java/com/example/util/NumberGeneratorTest.java
+++ src/test/java/com/example/util/NumberGeneratorTest.java
@@ -1,7 +1,11 @@
 package com.example.util;
 
+import com.example.domain.CardService;
+import com.example.domain.CardServiceImpl;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.Mockito;
 
 import java.util.Random;
 
@@ -13,12 +17,15 @@ class NumberGeneratorTest {
     private final String pinMatcher = "[0-9]{4}";
 
     private Random random;
+    @Mock
+    private CardService service;
     private Generator generator;
 
     @BeforeEach
     void setUp() {
         random = new Random();
-        generator = new NumberGenerator(random);
+        service = Mockito.mock(CardServiceImpl.class);
+        generator = new NumberGenerator(random, service);
     }
 
     @Test
