2f3c14f1d2655b25c1c58b95cc00bdaa6de2fda3
Bartek Dec <decbartek@gmail.com>
2020-09-01 18:22:28 +0200

    added CardService

---
+++ src/main/java/com/example/domain/CardServiceImpl.java
@@ -0,0 +1,31 @@
+package com.example.domain;
+
+import java.util.List;
+
+public class CardServiceImpl implements CardService {
+
+    private CardRepository repository;
+
+    public CardServiceImpl(CardRepository repository) {
+        this.repository = repository;
+    }
+
+    @Override
+    public Card findCard(String cardNumber) {
+        if (cardNumber == null) {
+            return null;
+        }
+
+        return repository.findCard(cardNumber);
+    }
+
+    @Override
+    public List<String> findAllCardNumbers() {
+        return repository.findAllCardNumbers();
+    }
+
+    @Override
+    public int addCard(Card card) {
+        return repository.addCard(card);
+    }
+}
